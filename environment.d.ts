0dad91c858480e44301702a0b03638287bf6c582
AvyKween <waldorfroque@hotmail.com>
2023-03-04 12:33:04 -0500

    Fixed identation on enviroment.d.ts file

--- environment.d.ts
+++ environment.d.ts
@@ -1,10 +1,10 @@
 declare global {
-    namespace NodeJS {
-      interface ProcessEnv {
-        PORT?: string;
-        MONGO_URI: string;
-      }
+  namespace NodeJS {
+    interface ProcessEnv {
+      PORT?: string;
+      MONGO_URI: string;
     }
   }
+}
   
-  export {}
+export {}
